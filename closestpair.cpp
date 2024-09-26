faa8a2dbb6e443d4e77130bf4fc3528638d9464a
LeahBragg <leahb2613@gmail.com>
2024-09-25 23:37:49 -0500

    fixed errors

--- closestpair.cpp
+++ closestpair.cpp
@@ -6,6 +6,7 @@
 #include <cmath>
 using namespace std;
 
+//given brute force approach
 Outcome brute(const vector<Point>& data) {
     Outcome best(data[0], data[1], distSquared(data[0],data[1]));
     for (unsigned int j = 1; j < data.size(); j++) {
@@ -19,15 +20,16 @@ Outcome brute(const vector<Point>& data) {
     return best;
 }
 
-
+//recursive divide and conquer 
 Outcome divideAndConquer(const vector<Point>& data, vector<int> dataX, vector<int> dataY,int start, int end){
-    int n = end - start;
-
+   
+    int n = end - start; //base case
     Outcome best;
-    if (n <= 10){ //once it gets down to 10% of og points 
-        Outcome best(data[dataX[start]], data[dataX[start + 1]], distSquared(data[dataX[start]],data[dataX[end]])); //use first two points 
-        for(unsigned int j = start; j <= end - 1; j++){ 
-            for(unsigned int k = j + 1; k <= end; k++){ 
+    if (n <= 10){ 
+        //brute force
+        Outcome best(data[dataX[start]], data[dataX[start + 1]], distSquared(data[dataX[start]], data[dataX[start + 1]])); //use first two points 
+        for(int j = start; j <= end - 1; j++){ 
+            for(int k = j + 1; k <= end; k++){ 
                 long long temp = distSquared(data[j], data[k]); 
                 if(temp < best.dsq){ 
                     best = Outcome(data[j], data[k], temp); 
@@ -37,29 +39,36 @@ Outcome divideAndConquer(const vector<Point>& data, vector<int> dataX, vector<in
         return best; 
     }
 
-    int mid = (end - start)/2 + start;
-    cout << mid << endl;
-    Outcome cp1 = divideAndConquer(data, dataX, dataY, 0, mid);
-    Outcome cp2 = divideAndConquer(data, dataX, dataY, mid + 1, n-1);
+    int mid = (end - start)/2 + start; //find median between end and start
+    Outcome cpLeft = divideAndConquer(data, dataX, dataY, start, mid);
+    Outcome cpRight = divideAndConquer(data, dataX, dataY, mid + 1, end);
 
-    //find minimum from Left and Right
-    if(cp1.dsq > cp2.dsq){
-        best.dsq = cp2.dsq;
-        best.p = cp2.p;
-        best.q = cp2.q;
+    //find minimum from Left and Right and set it to "best"
+    if(cpLeft.dsq > cpRight.dsq){
+        best.dsq = cpRight.dsq;
+        best.p = cpRight.p;
+        best.q = cpRight.q;
     } else {
-        best.dsq = cp1.dsq;
-        best.p = cp1.p;
-        best.q = cp1.q;
+        best.dsq = cpLeft.dsq;
+        best.p = cpLeft.p;
+        best.q = cpLeft.q;
     }
 
+    //loop vertically through points close to median
     for(unsigned int i = 0; i < dataY.size() - 1; i++){
-        if(data[dataY[i]].x - dataX[mid] <= sqrt(best.dsq) &&
-            data[dataY[i + 1]].x - dataX[mid] <= sqrt(best.dsq)){ //within range of the median
-            if(distSquared(data[dataY[i]], data[dataY[i + 1]]) < best.dsq){
-                best.dsq = distSquared(data[dataY[i]], data[dataY[i + 1]]);
-                best.p = data[dataY[i]];
-                best.q = data[dataY[i + 1]];
+
+        if(abs(data[dataY[i]].x - dataX[mid] <= sqrt(best.dsq))){ //if point is in rang of the median
+            for(unsigned int j = i + 1; j < dataY.size(); j++){ //loop upwards
+                if((abs(data[dataY[j]].x - dataX[mid] <= sqrt(best.dsq)))){ //if this point is also in range
+                    
+                    if(distSquared(data[dataY[i]], data[dataY[j]]) < best.dsq){//new minimum found
+                        best.dsq = distSquared(data[dataY[i]], data[dataY[j]]);
+                        best.p = data[dataY[i]];
+                        best.q = data[dataY[j]];
+                    } else { //stop looping, out of bounds
+                        break;
+                    }
+                }
             }
         }
     }
@@ -92,9 +101,7 @@ Outcome efficient(const vector<Point>& data) {
 
     vector<int> dataX = sortX(data);
     vector<int> dataY = sortY(data);
-        
-    cout << "\n";
-
+    
     //Divide and conquer
     Outcome best = divideAndConquer(data, dataX, dataY, 0, data.size() - 1);
 
