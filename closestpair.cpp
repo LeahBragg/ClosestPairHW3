9cbe006a26c09cb2d71af94de54958cd735c9cac
Leah B <36492786+LeahBragg@users.noreply.github.com>
2024-09-25 20:20:02 -0500

    Add files via upload

---
+++ closestpair.cpp
@@ -0,0 +1,35 @@
+#include "closestpair.h"
+#include <algorithm>
+#include <cstdlib>
+#include <iostream>
+//#include <sys/time.h>  // used to seed srand for extra credit
+using namespace std;
+
+Outcome brute(const vector<Point>& data) {
+    Outcome best(data[0], data[1], distSquared(data[0],data[1]));
+    for (unsigned int j=1; j < data.size(); j++) {
+        for (unsigned int k=0; k < j; k++) {
+            long long temp = distSquared(data[j], data[k]);
+            if (temp < best.dsq) {
+                best = Outcome(data[j], data[k], temp);
+            }
+        }
+    }
+    return best;
+}
+
+Outcome efficient(const vector<Point>& data) {
+    //sort by x
+    //sort by y   
+    std::sort(data.begin(), data.end(), compareByX);
+    for(int i = 0; i < sizeof(data); i++){
+        cout << data[i].x;
+    }
+    return Outcome();
+}
+
+Outcome extra(const vector<Point>& data) {
+    return Outcome();
+}
+
+
